eada34509ba0252199ebfaf71d83036ac2392438
KeerthivasanIITM <keerthivasan.de.villiers.17@gmail.com>
2024-01-12 01:14:50 +0530

    added a combined move group runner

--- b47_walking_control/scripts/combined.py
+++ b47_walking_control/scripts/combined.py
@@ -2,22 +2,53 @@
 import rospy
 import moveit_commander
 from geometry_msgs.msg import Pose
-import sys
 from gazebo_ros_link_attacher.srv import Attach, AttachRequest, AttachResponse
+import os
 
 class Move_Arm:
     def __init__(self):
-        joint_state_topic = ['joint_states:=/joint_states']
-        # moveit_commander.roscpp_initialize(joint_state_topic)
-        self.robot = moveit_commander.RobotCommander()
+        self.robot = moveit_commander.RobotCommander(robot_description="/robot_description")
         self.group = moveit_commander.MoveGroupCommander("arm")
-        # moveit_commander.roscpp_initialize(sys.argv)
         self.group.set_pose_reference_frame('base_link')
-        pass
+
+        '''HINT:http://docs.ros.org/en/noetic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html'''
+        
+        self.robot_inv = moveit_commander.RobotCommander()
+        self.group_inv = moveit_commander.MoveGroupCommander("inv_arm",ns="/inv_arm",robot_description="/inv_arm/robot_description")
+        self.group_inv.set_pose_reference_frame('link_05')
+
     
     def give_goal(self,p_x,p_y,p_z,x,y,z,w):
         end_effector_link = self.group.get_end_effector_link()
         current_pose = self.group.get_current_pose(end_effector_link).pose
+        print("End Effector Position:")
+        print("X:", current_pose.position.x)
+        print("Y:", current_pose.position.y)
+        print("Z:", current_pose.position.z)
+    
+        print("\nEnd Effector Orientation (Quaternion):")
+        print("X:", current_pose.orientation.x)
+        print("Y:", current_pose.orientation.y)
+        print("Z:", current_pose.orientation.z)
+        print("W:", current_pose.orientation.w)
+
+        pose_goal = Pose()
+        pose_goal.orientation.w = w
+        pose_goal.orientation.x = x
+        pose_goal.orientation.y = y
+        pose_goal.orientation.z = z
+        pose_goal.position.x = p_x
+        pose_goal.position.y = p_y
+        pose_goal.position.z = p_z
+        self.group.set_pose_target(pose_goal)
+        plan = self.group.go(wait=True)
+        self.group.stop()
+        self.group.clear_pose_targets()
+
+    
+    def give_goal_inv(self,p_x,p_y,p_z,x,y,z,w):
+        end_effector_link = self.group_inv.get_end_effector_link()
+        current_pose = self.group_inv.get_current_pose(end_effector_link).pose
         print("End Effector Position:")
         print("X:", current_pose.position.x)
         print("Y:", current_pose.position.y)
@@ -67,11 +98,15 @@ class Attacher:
         req.link_name_2 = link_name_2
         self.detach_srv.call(req)
 
-
 if __name__ == "__main__":
-    rospy.init_node("move_arm")
+    rospy.init_node("combined")
     rospy.loginfo("Starting move_arm")
     attacher = Attacher()
-    attacher.attach_request("unit_box","link","robot","base_link")
+    attacher.attach_request("test_surface","link","robot","base_link")
     move_arm = Move_Arm()
-    move_arm.give_goal(1.3936,-0.10464,0.2031,0,1,0,0)
+    move_arm.give_goal(1.3936,0,0.2031,0,1,0,0)
+    attacher.detach_request("test_surface","link","robot","base_link")
+    attacher.attach_request("test_surface","link","robot","link_05")
+    move_arm = Move_Arm()
+    move_arm.give_goal_inv(-1.3936,0,0.2031,0,1,0,0)
+    attacher.detach_request("test_surface","link","robot","link_05")
